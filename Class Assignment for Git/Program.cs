a8be7ff77ce23f8f5e5a6afb04397c8fb56007df
Adam <adachis@ut.utm.edu>
2017-09-11 16:12:20 -0500

    Makes the program a random number guessing game.

--- Class Assignment for Git/Program.cs
+++ Class Assignment for Git/Program.cs
@@ -10,7 +10,32 @@ namespace Class_Assignment_for_Git
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Random rand = new Random();
+            int secretNum = rand.Next(0, 100);
+            int userGuess = -1;
+            int guessCounter = 0;
+            string winMessage;
+
+            Console.WriteLine("I'm thinking of a number between 1 and 100.");
+            while (userGuess != secretNum)
+            {
+                Console.Write("Guess: ");
+                userGuess = Convert.ToInt32(Console.ReadLine());
+                if (userGuess < secretNum)
+                {
+                    Console.WriteLine("Too low.");
+                }
+                else if (userGuess > secretNum)
+                {
+                    Console.WriteLine("Too high.");
+                }
+                guessCounter += 1;
+            }
+            winMessage = String.Format("You won in {0} guesses!", guessCounter);
+            Console.WriteLine(winMessage);
+
+            Console.WriteLine("Enter anything to exit.");
+            Console.ReadLine();
         }
     }
 }
